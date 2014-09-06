6afcba91cf141f8989a0640d0f328dca9b8a9d8a
Tim C. Schroeder <tim@blitzcode.net>
2014-09-06 18:23:43 +0200

    Added PrintBytesHumanReadable() to Trace module

--- trace.cpp
+++ trace.cpp
@@ -4,7 +4,6 @@
 #include <cstdio>
 #include <cstdarg>
 #include <thread>
-#include <string>
 #include <sstream>
 
 #include "timer.h"
@@ -24,5 +23,23 @@ void Trace(const char *fmt, ...)
 	va_end(argp);
 
     std::printf("%-14s @ %6.2fs - %s\n", stream.str().c_str(), tick, fmt_buf);
+}
+
+std::string PrintBytesHumanReadable(uint64 bytes)
+{
+    // Convert byte count into more compact and readable KB / MB / GB string
+
+    char buf[64];
+
+    if (bytes < 1024)
+        std::snprintf(buf, sizeof(buf), "%iB", int(bytes));
+    else if (bytes < 1024 * 1024)
+        std::snprintf(buf, sizeof(buf), "%.1fKB", float(double(bytes) / 1024.0));
+    else if (bytes < 1024 * 1024 * 1024)
+        std::snprintf(buf, sizeof(buf), "%.1fMB", float(double(bytes) / 1024.0 / 1024.0));
+    else
+        std::snprintf(buf, sizeof(buf), "%.2fGB", float(double(bytes) / 1024.0 / 1024.0 / 1024.0));
+
+    return std::string(buf);
 }
 
--- trace.h
+++ trace.h
@@ -2,7 +2,12 @@
 #ifndef TRACE_H
 #define TRACE_H
 
+#include <string>
+
+#include "types.h"
+
 void Trace(const char *fmt, ...);
+std::string PrintBytesHumanReadable(uint64 bytes);
 
 #endif // TRACE_H
 
