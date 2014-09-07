89c3a502e2c86983a8bf0d9acc05039957300cd7
Tim C. Schroeder <tim@blitzcode.net>
2014-09-07 12:48:03 +0200

    Added private section to Tile and Framebuffer, removing access to as much implementation detail as possible

--- framebuffer.h
+++ framebuffer.h
@@ -25,11 +25,10 @@ public:
     void RestartRendering();
 
 protected:
-    uint m_width                = 1;
-    uint m_height               = 1;
-    static const uint m_tiles_x = 12;
-    static const uint m_tiles_y = 9;
-    const uint m_num_cpus;
+    uint m_width  = 1;
+    uint m_height = 1;
+
+    volatile bool m_threads_stop = false; // Stop signal for the worker threads
 
     class Tile
     {
@@ -37,13 +36,18 @@ protected:
         Tile();
         ~Tile() { glDeleteTextures(1, &m_tex); }
 
+        // Minimal interface needed by implementors of RenderTile()
         void GetPosition(uint& x0, uint& y0, uint& x1, uint& y1) const
             { x0 = m_x0; y0 = m_y0; x1 = m_x1; y1 = m_y1; }
-        void SetPosition(uint x0, uint y0, uint x1, uint y1);
-
         uint GetWidth()  const { return m_x1 - m_x0; }
         uint GetHeight() const { return m_y1 - m_y0; }
         uint32 * GetBuffer()   { return &m_bgra[0];  }
+
+    protected:
+        // Hide most accessors from Framebuffer derived classes
+        friend class Framebuffer;
+
+        void SetPosition(uint x0, uint y0, uint x1, uint y1);
         void Clear();
 
         bool GetDirty() const     { return m_dirty;  }
@@ -53,33 +57,39 @@ protected:
 
         std::mutex& GetMutex() { return m_mtx; }
 
-    protected:
-        std::mutex          m_mtx;
+    private:
+        std::mutex          m_mtx;      // Worker threads will lock while writing to buffer
         GLuint              m_tex = -1;
-        std::vector<uint32> m_bgra;
-        bool                m_dirty; // Does the texture different from the buffer?
+        std::vector<uint32> m_bgra;     // Buffer
+        bool                m_dirty;    // Does the texture different from the buffer?
         uint                m_x0;
         uint                m_y0;
         uint                m_x1;
         uint                m_y1;
     };
 
-    std::array<Tile, m_tiles_x * m_tiles_y> m_tiles;
+    // Override to provide actual rendering functionality
+    virtual void RenderTile(Tile& tile) = 0;
 
+private:
     std::vector<uint> m_work_queue; // Indices of tiles left to render
     std::mutex m_work_queue_mtx;    // Mutex to control work queue access
-    Tile * GetNextTileFromQueue();
 
-    volatile bool m_threads_stop = false; // Stop signal for the worker threads
-    std::vector<std::thread> m_threads;   // Array of worker threads
-    std::atomic<uint> m_threads_done;     // Number of threads done
-    double m_render_start_time = 0.0;     // Time at which the worker threads started rendering
+    static const uint m_tiles_x = 12; // Tile layout and number of worker threads fixed
+    static const uint m_tiles_y = 9;  // ...
+    const uint m_num_cpus;            // ...
+
+    std::array<Tile, m_tiles_x * m_tiles_y> m_tiles;
+    Tile * GetNextTileFromQueue(); // Queried by WorkerThread()
+    void FillWorkQueue();
+
+    std::vector<std::thread> m_threads; // Array of worker threads
+    std::atomic<uint> m_threads_done;   // Number of threads done
+    double m_render_start_time = 0.0;   // Time at which the worker threads started rendering
+
     void WorkerThread();
     void KillAllWorkerThreads();
     void CreateWorkerThreads();
-    void FillWorkQueue();
-
-    virtual void RenderTile(Tile& tile) = 0;
 };
 
 #endif // FRAMEBUFFER_H
