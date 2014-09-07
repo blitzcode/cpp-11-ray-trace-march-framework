55bf84c145281941866a70113982fc81a2f0ccd6
Tim C. Schroeder <tim@blitzcode.net>
2014-09-07 11:23:04 +0200

    Inlined some code into the header, removed .cpp files

--- camera.h
+++ camera.h
@@ -5,15 +5,46 @@
 #include "types.h"
 #include "lin_alg.h"
 
-void GenerateRay(const Matrix44f& camera, // Camera transform
-                 Vec2ui pixel,            // Screen pixel
-                 uint width,              // Screen width
-                 uint height,             // ...and height
-                 Vec2f sample_offs,       // Sample offset [-.5, +.5]
-                 bool ortho,              // Orthographic or perspective camera?
-                 float width_or_hfov,     // Width of ortho viewing volume or horizontal FOV degrees
-                 Vec3f& origin,
-                 Vec3f& dir);
+inline void GenerateRay(
+    const Matrix44f& camera, // Camera transform
+    Vec2ui pixel,            // Screen pixel
+    uint width,              // Screen width
+    uint height,             // ...and height
+    Vec2f sample_offs,       // Sample offset [-.5, +.5]
+    bool ortho,              // Orthographic or perspective camera?
+    float width_or_hfov,     // Width of ortho viewing volume or horizontal FOV degrees
+    Vec3f& origin,
+    Vec3f& dir)
+{
+    // Convert fragment coordinates and sample offset to NDC [-1, 1]
+    const Vec2f ndc((pixel.x + sample_offs.x) / float(width)  * 2.0f - 1.0f,
+                    (pixel.y + sample_offs.y) / float(height) * 2.0f - 1.0f);
+
+    // Generate ray from NDC and camera transform
+    const float aspect = float(width) / float(height);
+    if (ortho)
+    {
+        // Orthographic projection. Frame [-w/2, w/2] on X,
+        // center interval on Y while keeping aspect
+        const float width  = width_or_hfov;
+        const float height = float(width) / aspect;
+        camera.Transf4x4(Vec3f(ndc.x * (float(width ) / 2.0),
+                               ndc.y * (float(height) / 2.0),
+                               0.0),
+                         origin);
+        camera.TransfNormal3x3(Vec3f(0.0f, 0.0f, -1.0f), dir);
+    }
+    else
+    {
+        // Perspective projection. Unlike the usual vertical FOV we deal with a horizontal
+        // one, just like the orthographic camera defined by its width
+        const float hfov   = DegToRad(width_or_hfov);
+        const float fov_xs = tan(hfov / 2);
+        camera.Transf4x4(Vec3f(0.0), origin);
+        camera.TransfNormal3x3
+            (Normalize(Vec3f(ndc.x * fov_xs, ndc.y * fov_xs / aspect, -1.0f)), dir);
+    }
+}
 
 #endif // CAMERA_H
 
--- triangle.h
+++ triangle.h
@@ -9,14 +9,69 @@
 // http://www.jcenligne.fr/download/little3d/
 //     jgt%20Fast,%20Minumum%20Storage%20Ray-Triangle%20Intersection.htm
 //
-bool IntersectRayTri(const Vec3f& origin,
-                     const Vec3f& dir,
-                     const Vec3f& v0,
-                     const Vec3f& v1,
-                     const Vec3f& v2,
-                     float& t,
-                     float& u,
-                     float& v);
+inline bool IntersectRayTri(const Vec3f& origin,
+                            const Vec3f& dir,
+                            const Vec3f& vert0,
+                            const Vec3f& vert1,
+                            const Vec3f& vert2,
+                            float& t,
+                            float& u,
+                            float& v)
+{
+    #define EPSILON 0.000001
+    #define CROSS(dest,v1,v2) \
+              dest[0]=v1[1]*v2[2]-v1[2]*v2[1]; \
+              dest[1]=v1[2]*v2[0]-v1[0]*v2[2]; \
+              dest[2]=v1[0]*v2[1]-v1[1]*v2[0];
+    #define DOT(v1,v2) (v1[0]*v2[0]+v1[1]*v2[1]+v1[2]*v2[2])
+    #define SUB(dest,v1,v2) \
+              dest[0]=v1[0]-v2[0]; \
+              dest[1]=v1[1]-v2[1]; \
+              dest[2]=v1[2]-v2[2];
+
+    float edge1[3], edge2[3], tvec[3], pvec[3], qvec[3];
+    float det,inv_det;
+
+    /* find vectors for two edges sharing vert0 */
+    SUB(edge1, vert1, vert0);
+    SUB(edge2, vert2, vert0);
+
+    /* begin calculating determinant - also used to calculate U parameter */
+    CROSS(pvec, dir, edge2);
+
+    /* if determinant is near zero, ray lies in plane of triangle */
+    det = DOT(edge1, pvec);
+
+    if (det > -EPSILON && det < EPSILON)
+        return false;
+    inv_det = 1.0 / det;
+
+    /* calculate distance from vert0 to ray origin */
+    SUB(tvec, origin, vert0);
+
+    /* calculate U parameter and test bounds */
+    u = DOT(tvec, pvec) * inv_det;
+    if (u < 0.0 || u > 1.0)
+        return false;
+
+    /* prepare to test V parameter */
+    CROSS(qvec, tvec, edge1);
+
+    /* calculate V parameter and test bounds */
+    v = DOT(dir, qvec) * inv_det;
+    if (v < 0.0 || u + v > 1.0)
+        return false;
+
+    /* calculate t, ray intersects triangle */
+    t = DOT(edge2, qvec) * inv_det;
+
+    return true;
+
+    #undef EPSILON
+    #undef CROSS
+    #undef DOT
+    #undef SUB
+}
 
 template<typename T> Vector_t<T, 3> TriangleNormal(const Vector_t<T, 3>& v0,
                                                    const Vector_t<T, 3>& v1,
